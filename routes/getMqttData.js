f735cf94d8af26b5743fb1c7f3ff14e329dac4bc
kaleemullah360 <kaleemullah360@live.com>
2016-04-20 12:42:38 +0500

    updates

--- routes/getMqttData.js
+++ routes/getMqttData.js
@@ -14,23 +14,24 @@ var connection = mysql.createConnection({
   database : 'e-mch' // app database name
 });
 connection.connect();
-
+/*
 var mqtt = require('mqtt')
 , client = mqtt.connect();
 client.subscribe('iot-2/evt/status/fmt/json');
-
+*/
 /* GET MQTT Data. */
 //	http://localhost:3000/getMqttData?uri=aaaa::c30c:0:0:4
 router.get('/', function(req, res, next) {
 	var mote_uri = req.query.uri;
 	var start = new Date();
+	/*
 	client.on('message', function(topic, payload) {
 		var RTT = new Date() - start;
 		start = new Date();
 		//console.info("Execution time: %dms", RTT);
 		m_payload = decoder.write(payload);
 		//client.end();
-
+		console.log(m_payload);
 		//  populate database
 	      //  MessageID, UpTime, ClockTime, Temperature, Battery, PowTrace  //<-- This
 	      var string = "";
@@ -42,11 +43,11 @@ router.get('/', function(req, res, next) {
 	      var Temperature = string[3];
 	      var Battery = string[4];
 	      var PowTrace = string[5];
-	      connection.query('INSERT INTO `e-mch-table` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
+	      /*connection.query('INSERT INTO `e-mch-table` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
 	      	if (err) throw err;
 	      });
 
-	});
+	}); */
 res.send(m_payload);
 });
 
