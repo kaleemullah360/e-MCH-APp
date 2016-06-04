b7f6203925bcad4ef0f09d746804f4f71b40aa69
kaleemullah360 <kaleemullah360@live.com>
2016-06-04 13:38:01 +0500

    updates

--- routes/getCoapData.js
+++ routes/getCoapData.js
@@ -31,40 +31,42 @@ router.get('/', function(req, res, next) {
 	var duration_sec = req.query.d;
 	var n_hops = req.query.h;
     // MQTT_0.5Sec_3Hop
-	var Protocol = 'CoAP'+ duration_sec +'Sec_'+ n_hops +'Hop';
-	var start = new Date();
-	var c_req = coap.request('coap://[' + mote_uri + ']:5683/sens/mote')
-	c_req.on('response', function(c_res) {
-		var RTT = new Date() - start;
+    var Protocol = 'CoAP'+ duration_sec +'Sec_'+ n_hops +'Hop';
+    var start = new Date();
+    var c_req = coap.request('coap://[' + mote_uri + ']:5683/sens/mote')
+    c_req.on('response', function(c_res) {
+    	var RTT = new Date() - start;
 		//console.info("Execution time: %dms", RTT);
-		if (c_res.payload.length < 5) return;
-		c_payload = decoder.write(c_res.payload);
+		if (!c_res.payload){
+			return;	
+		}
+			c_payload = decoder.write(c_res.payload);
 		//console.log(c_res.payload);
 		//  populate database
 	      //  MessageID, UpTime, ClockTime, Temperature, Battery, PowTrace  //<-- This
 	      var string = "";
-	    string =String(c_payload);
-	    string = string.split(",");
-		MessageID   = (string[0]) ? string[0] : '0' ;
-		UpTime      = (string[1]) ? string[1] : '0' ;
-		ClockTime   = (string[2]) ? string[2] : '0' ;
-		Temperature = (string[3]) ? string[3] : '0' ;
-		Battery     = (string[4]) ? string[4] : '0' ;
-		PowTrace    = (string[5]) ? string[5] : '0' ;
-	    connection.query('INSERT INTO `emch-tbl` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
-	      if (err) throw err;
-	    });
+	      string =String(c_payload);
+	      string = string.split(",");
+	      MessageID   = (string[0]) ? string[0] : '0' ;
+	      UpTime      = (string[1]) ? string[1] : '0' ;
+	      ClockTime   = (string[2]) ? string[2] : '0' ;
+	      Temperature = (string[3]) ? string[3] : '0' ;
+	      Battery     = (string[4]) ? string[4] : '0' ;
+	      PowTrace    = (string[5]) ? string[5] : '0' ;
+	      connection.query('INSERT INTO `emch-tbl` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
+	      	if (err) throw err;
+	      });
 
-		res.send(c_payload);
-	})
-	c_req.on('error', function(c_res) {
-	request_counter = request_counter + 1;
-    console.log("################### " + request_counter + " ###################\n");
-    console.log(c_res);
-    console.log("######################################\n");
-    return;
-	})
-	c_req.end()
+	      res.send(c_payload);
+	  })
+    c_req.on('error', function(c_res) {
+    	request_counter = request_counter + 1;
+    	console.log("################### " + request_counter + " ###################\n");
+    	console.log(c_res);
+    	console.log("######################################\n");
+    	return;
+    })
+    c_req.end()
 });
 
 module.exports = router;
