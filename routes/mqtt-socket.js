d3bd199aab4eba5fd06bf376cf9c16611bfa9236
kaleemullah360 <kaleemullah360@live.com>
2016-08-07 17:06:28 +0500

    updates

--- routes/mqtt-socket.js
+++ routes/mqtt-socket.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+// auto uodate webpage
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('mqtt-socket', { title: 'MQTT-Socket' });
