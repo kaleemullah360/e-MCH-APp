a1913a9cf6d05bf099c1218c1cd1d840a633a346
kaleemullah360 <kaleemullah360@live.com>
2016-10-07 12:21:19 +0500

    updates

--- config/dbcon.js
+++ config/dbcon.js
@@ -1,13 +1,19 @@
+/**
+ * MySQL Connection Module.
+ * wirtten and placed in created 'config' directory by Kaleem Ullah
+ */
+
 // create MYSQL Server connection to store data
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-			host     : 'localhost', // default
-			user     : 'root',  // default
-			password : '',  // default
-			database : 'e-mch-db' // app database name
+			host     : 'localhost', 	// default-localhost
+			user     : 'root',  			// default-root-user
+			password : '',  					// default-no-password
+			database : 'e-mch-db' 		// app database name
 });
 connection.connect(function(err) {
     if (err) throw err;
 });
 
+// export this module for importing in routes
 module.exports = connection;
