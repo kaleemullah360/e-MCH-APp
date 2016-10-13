f770fdcf3ed04c1998c0a775cc44dea544c6a211
kaleemullah360 <kaleemullah360@live.com>
2016-10-13 13:47:08 +0500

    updates

--- config/ping.js
+++ config/ping.js
@@ -7,9 +7,21 @@
 var ping = require ("net-ping");
 var session = ping.createSession (options);
 
+/*-------------------- PING Lib Configs ---------------------*/
+// Default options
+var options = {
+  networkProtocol: ping.NetworkProtocol.IPv6,
+  packetSize: 64,
+  retries: 1,
+  sessionId: (process.pid % 65535),
+  timeout: 10000,
+  ttl: 128
+};
+var session = ping.createSession (options);
+
 session.on ("error", function (error) {
   console.trace (error.toString ());
 });
-
+/*-------------------- End PING Lib Configs ------------------*/
 // export this module for importing in routes
 module.exports = session;
