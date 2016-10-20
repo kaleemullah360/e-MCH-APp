69068a81053936963308698a7b7ca877b93639a3
kaleemullah360 <kaleemullah360@live.com>
2016-10-20 13:23:44 +0500

    updates

--- routes/getHttpData.js
+++ routes/getHttpData.js
@@ -1,8 +1,8 @@
-var express 		= require('express');
-var session  		= require('../config/ping'); // include ping configs && path must be relative to file you're in
+var express 	= require('express');
+var session  	= require('../config/ping'); // include ping configs && path must be relative to file you're in
 var connection  = require('../config/dbcon'); // include mysql connection object && path must be relative to file you're in
-var router 			= express.Router();
-var mote_uri 		= 'aaaa::c30c:0:0:3';
+var router 		= express.Router();
+var mote_uri 	= 'aaaa::c30c:0:0:3';
 
 // variables
 var MessageID   = "nil";
@@ -15,67 +15,67 @@ var RTT         = "nil";
 
 var request_counter = 1;
 const StringDecoder = require('string_decoder').StringDecoder;
-const decoder 			= new StringDecoder('utf8');
+const decoder 		= new StringDecoder('utf8');
 
 // for making reuest
-var request 				= require('request');
+var request 		= require('request');
 
 /* GET HTTP Data. */
 //	http://localhost:3000/getHttpData?uri=aaaa::c30c:0:0:3
 router.get('/', function(req, res, next) {
-	var mote_uri 			= req.query.uri;
+	var mote_uri 		= req.query.uri;
 	var duration_sec 	= req.query.d;
-	var n_hops 				= req.query.h;
+	var n_hops 			= req.query.h;
 
-  /*-------------------- get Round Trip Time ---------------------*/
-  session.pingHost (mote_uri, function (rtt_error, mote_uri, sent, rcvd) {
-    RTT = rcvd - sent;
-    //console.log ("Target " + mote_uri + ": RTT (ms=" + RTT + ")");
+	/*-------------------- get Round Trip Time ---------------------*/
+	session.pingHost (mote_uri, function (rtt_error, mote_uri, sent, rcvd) {
+		RTT = rcvd - sent;
+//console.log ("Target " + mote_uri + ": RTT (ms=" + RTT + ")");
 
-    if(!rtt_error){
-      /*-------------------- get Payload ---------------------*/
-    // HTTP_0.5Sec_3Hop
-    var Protocol = 'HTTP_'+ duration_sec +'Sec_'+ n_hops +'Hop';
-    request('http://['+mote_uri+']', function (request_error, response, payload) {
+if(!rtt_error){
+	/*-------------------- get Payload ---------------------*/
+// HTTP_0.5Sec_3Hop
+var Protocol = 'HTTP_'+ duration_sec +'Sec_'+ n_hops +'Hop';
+request('http://['+mote_uri+']', function (request_error, response, payload) {
 
-     if (request_error) {
-       request_counter = request_counter + 1;
-       console.log("[===============< " + request_counter + " >===============]\n");
-       console.log(request_error);
-       console.log("[==================================]\n");
-       return;
-     }
+	if (request_error) {
+		request_counter = request_counter + 1;
+		console.log("[===============< " + request_counter + " >===============]\n");
+		console.log(request_error);
+		console.log("[==================================]\n");
+		return;
+	}
 
-     if (payload) {
-      h_payload 	= decoder.write(payload);
-				//  populate database
-      //  MessageID, UpTime, ClockTime, Temperature, Battery, PowTrace  //<-- This
-      var string 	= "";
-      string 			=	String(h_payload);
-      string 			= string.split(",");
-      MessageID   = (string[0]) ? string[0] : '0' ;
-      UpTime      = (string[1]) ? string[1] : '0' ;
-      ClockTime   = (string[2]) ? string[2] : '0' ;
-      Temperature = (string[3]) ? string[3] : '0' ;
-      Battery     = (string[4]) ? string[4] : '0' ;
-      PowTrace    = (string[5]) ? string[5] : '0' ;
-      connection.query('INSERT INTO `emch-tbl` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
-      	if (err) throw err;
-      });
-      console.log("Data received  " + h_payload + "\n")
-      res.send(h_payload + "," + RTT);
+	if (payload) {
+		h_payload 	= decoder.write(payload);
+//  populate database
+//  MessageID, UpTime, ClockTime, Temperature, Battery, PowTrace  //<-- This
+var string 	= "";
+string 			=	String(h_payload);
+string 			= string.split(",");
+MessageID   = (string[0]) ? string[0] : '0' ;
+UpTime      = (string[1]) ? string[1] : '0' ;
+ClockTime   = (string[2]) ? string[2] : '0' ;
+Temperature = (string[3]) ? string[3] : '0' ;
+Battery     = (string[4]) ? string[4] : '0' ;
+PowTrace    = (string[5]) ? string[5] : '0' ;
+connection.query('INSERT INTO `emch-tbl` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
+	if (err) throw err;
+});
+console.log("Data received  " + h_payload + "\n")
+res.send(h_payload + "," + RTT);
 
-    }else{return}
-  })
+}else{return}
+})
 
-    /*-------------------- End get Payload ---------------------*/
-  }else{
-    console.log("HTTP: Ping failed, Device is not reachable, Trying again ... \n");
-  return; // No RTT
+/*-------------------- End get Payload ---------------------*/
+}else{
+	console.log("HTTP: Ping failed, Device is not reachable, Trying again ... \n");
+return; // No RTT
 }
 
 });
-  /*-------------------- End get Round Trip Time ---------------------*/
+	/*-------------------- End get Round Trip Time ---------------------*/
 });
 
 module.exports = router;
