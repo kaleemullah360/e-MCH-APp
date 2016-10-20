0de03313411339d853d836b7f5be5210d3c234cd
kaleemullah360 <kaleemullah360@live.com>
2016-10-20 13:25:56 +0500

    updates

--- routes/getCoapData.js
+++ routes/getCoapData.js
@@ -1,5 +1,5 @@
 var express     = require('express');
-var session  		= require('../config/ping'); // include ping configs && path must be relative to file you're in
+var session  	= require('../config/ping'); // include ping configs && path must be relative to file you're in
 var connection  = require('../config/dbcon'); // include mysql connection object && path must be relative to file you're in
 var router      = express.Router();
 var mote_uri    = 'aaaa::c30c:0:0:2';
@@ -21,61 +21,61 @@ var coap            = require('coap')
 /* GET CoAP Data. */
 //	http://localhost:3000/getCoapData?uri=aaaa::c30c:0:0:2
 router.get('/', function(req, res, next) {
-    var mote_uri    = req.query.uri;
-    var duration_sec= req.query.d;
-    var n_hops      = req.query.h;
+	var mote_uri    = req.query.uri;
+	var duration_sec= req.query.d;
+	var n_hops      = req.query.h;
 
-    /*-------------------- get Round Trip Time ---------------------*/
-    session.pingHost (mote_uri, function (rtt_error, mote_uri, sent, rcvd) {
-        RTT = rcvd - sent;
-        //console.log ("Target " + mote_uri + ": RTT (ms=" + RTT + ")");
+	/*-------------------- get Round Trip Time ---------------------*/
+	session.pingHost (mote_uri, function (rtt_error, mote_uri, sent, rcvd) {
+		RTT = rcvd - sent;
+		//console.log ("Target " + mote_uri + ": RTT (ms=" + RTT + ")");
 
-        if(!rtt_error){
-            /*-------------------- get Payload ---------------------*/
-        // CoAP_0.5Sec_3Hop
-        var Protocol    = 'CoAP_'+ duration_sec +'Sec_'+ n_hops +'Hop';
-        var c_req       = coap.request('coap://[' + mote_uri + ']:5683/sens/mote')
-        c_req.on('response', function(c_res) {
-            //console.info("RTT: %dms", RTT);
-            if (!c_res.payload){
-                return;	
-            }
-            c_payload   = decoder.write(c_res.payload);
-            //  populate database
-            //  MessageID, UpTime, ClockTime, Temperature, Battery, PowTrace  //<-- This
-            var string  = "";
-            
-            string      = String(c_payload);
-            string      = string.split(",");
-            MessageID   = (string[0]) ? string[0] : '0' ;
-            UpTime      = (string[1]) ? string[1] : '0' ;
-            ClockTime   = (string[2]) ? string[2] : '0' ;
-            Temperature = (string[3]) ? string[3] : '0' ;
-            Battery     = (string[4]) ? string[4] : '0' ;
-            PowTrace    = (string[5]) ? string[5] : '0' ;
-            connection.query('INSERT INTO `emch-tbl` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
-              if (err) throw err;
-          });
-            console.log("Data received  " + c_payload + "\n")
-            res.send(c_payload + "," + RTT);
-        })
-        c_req.on('error', function(c_res) {
-            request_counter = request_counter + 1;
-            console.log("[===============< " + request_counter + " >===============]\n");
-            console.log(c_res);
-            console.log("[==================================]\n");
-            return;
-        })
-        c_req.end()
+		if(!rtt_error){
+			/*-------------------- get Payload ---------------------*/
+		// CoAP_0.5Sec_3Hop
+		var Protocol  = 'CoAP_'+ duration_sec +'Sec_'+ n_hops +'Hop';
+		var c_req     = coap.request('coap://[' + mote_uri + ']:5683/sens/mote')
+		c_req.on('response', function(c_res) {
+		//console.info("RTT: %dms", RTT);
+		if (!c_res.payload){
+			return;	
+		}
+		c_payload   = decoder.write(c_res.payload);
+		//  populate database
+		//  MessageID, UpTime, ClockTime, Temperature, Battery, PowTrace  //<-- This
+		var string  = "";
 
-        /*-------------------- End get Payload ---------------------*/
-    }else{
-      console.log("CoAP: Ping failed, Device is not reachable, Trying again ... \n");
-          return;	// No RTT
-      }
+		string      = String(c_payload);
+		string      = string.split(",");
+		MessageID   = (string[0]) ? string[0] : '0' ;
+		UpTime      = (string[1]) ? string[1] : '0' ;
+		ClockTime   = (string[2]) ? string[2] : '0' ;
+		Temperature = (string[3]) ? string[3] : '0' ;
+		Battery     = (string[4]) ? string[4] : '0' ;
+		PowTrace    = (string[5]) ? string[5] : '0' ;
+		connection.query('INSERT INTO `emch-tbl` (MessageID, UpTime, ClockTime, Temperature, Battery, Protocol, RTT, PowTrace) VALUES (\''+MessageID+'\',\''+UpTime+'\', \''+ClockTime+'\', \''+Temperature+'\', \''+Battery+'\', \''+Protocol+'\', \''+RTT+'\', \''+PowTrace+'\')', function(err, rows, fields) {
+			if (err) throw err;
+		});
+		console.log("Data received  " + c_payload + "\n")
+		res.send(c_payload + "," + RTT);
+		})
+		c_req.on('error', function(c_res) {
+			request_counter = request_counter + 1;
+			console.log("[===============< " + request_counter + " >===============]\n");
+			console.log(c_res);
+			console.log("[==================================]\n");
+			return;
+		})
+		c_req.end()
 
-  });
-    /*-------------------- End get Round Trip Time ---------------------*/
+		/*-------------------- End get Payload ---------------------*/
+		}else{
+			console.log("CoAP: Ping failed, Device is not reachable, Trying again ... \n");
+		return;	// No RTT
+		}
+
+});
+	/*-------------------- End get Round Trip Time ---------------------*/
 });
 
 module.exports = router;
